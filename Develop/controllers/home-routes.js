93ad05fcde55d78f8eac589dd8b9cf59a8f7e139
SBC <lochan.sharad@gmail.com>
2021-07-22 23:40:26 +0800

    Controllers added and routes defined. Models created

---
+++ Develop/controllers/home-routes.js
@@ -0,0 +1,15 @@
+const { route } = require("./api");
+
+const router = require("express").Router();
+
+// const { Blog, User, Comment } = require("../models");
+
+// router.get("/", (req, res) => {
+//     db.Book.find({})
+//       .then(dbBook => {
+//         res.json(dbBook);
+//       })
+//       .catch(err => {
+//         res.json(err);
+//       });
+//   });
