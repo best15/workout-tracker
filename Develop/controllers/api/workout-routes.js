18909c38728c4718108f410263073e540e2e1120
SBC <lochan.sharad@gmail.com>
2021-07-23 19:53:18 +0800

    get routes for api/workouts/range added.

--- Develop/controllers/api/workout-routes.js
+++ Develop/controllers/api/workout-routes.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 
 const db = require("../../models");
 
+//get api/workouts
 router.get("/", async (req, res) => {
-
 
     try {
 
@@ -23,5 +23,34 @@ router.get("/", async (req, res) => {
         res.status(500).json(error);
     }
 });
+
+
+//get aapi/workouts/range
+router.get("/range", async (req, res) => {
+    try {
+
+        const pastSevenWorkouts = await db.Workout.aggregate([
+            {
+                $addFields: {
+                    totalDuration: {
+                        $sum: `$exercises.duration`,
+                    },
+                    combinedWeight: {
+                        $sum: `exercises.weight`,
+                    }
+                },
+
+            },
+            { $sort: { day: -1 } },
+
+        ]).limit(7);
+
+        res.status(200).json(pastSevenWorkouts);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+});
+
 
 module.exports = router;
