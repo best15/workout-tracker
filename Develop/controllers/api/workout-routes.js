ed0a3ad50a691f389f2ee1f175085a124c533a24
SBC <lochan.sharad@gmail.com>
2021-07-23 19:29:47 +0800

    get request for api/workouts handled in workout-routes.js

--- Develop/controllers/api/workout-routes.js
+++ Develop/controllers/api/workout-routes.js
@@ -3,8 +3,25 @@ const router = require("express").Router();
 const db = require("../../models");
 
 router.get("/", async (req, res) => {
-    console.log("api/workouts");
 
+
+    try {
+
+        const allWorkouts = await db.Workout.aggregate([
+            {
+                $addFields: {
+                    totalDuration: {
+                        $sum: `$exercises.duration`,
+                    },
+                },
+            },
+        ]);
+        console.log("allWorkouts------", allWorkouts);
+        res.status(200).json(allWorkouts);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
 });
 
 module.exports = router;
